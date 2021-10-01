e6a29ed6245a545af9bd2d73f29f2d0ffa4b5ca0
Mario Barbosa <mariogomesbarbosa@gmail.com>
2021-10-01 16:00:47 -0300

    Finishing side menu visual

--- components/SideMenu.js
+++ components/SideMenu.js
@@ -1,7 +1,37 @@
+import { useState, useEffect } from 'react'
 import styles from '../styles/SideMenu.module.css'
+import Image from 'next/image'
 
 export default function SideMenu () {
+  const [deckList, setDeckList] = useState([])
+
+  useEffect(() => {
+    const loadAllDecks = async () => {
+      // Getting all deck lists
+      let list = ['Inglês', 'Japonês', 'Alemão']
+      setDeckList(list)
+    }
+    
+    loadAllDecks()
+  }, [])
+
   return (
-    <div className={styles.sideMenu}>Menu bonitao</div>
+    <div className={styles.sideMenu}>
+      <div className={styles.menuHeader}>
+        <Image src="/flashee-icon.svg" alt="Flashee Logo" width={153} height={46} />
+      </div>
+      <div className={styles.menuItens}>
+        <div className={styles.menuTitle}>Baralhos</div>
+        {deckList.length > 0 ?
+          deckList.map(DeckItem => {
+            return (
+              <div className={styles.deckItens}>{DeckItem}</div>
+            )
+          })
+          :
+          <div className={styles.placeholder}>Nenhum baralho criado</div>
+        }
+      </div>
+    </div>
   )
 }
